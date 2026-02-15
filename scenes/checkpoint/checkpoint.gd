659ba3ed0dd87a738c179383e70674e95b1bcc09
Andrea Polanco <alpm9614@hotmail.com>
2026-02-15 15:59:01 -0400

    removing empty spaces

--- scenes/checkpoint/checkpoint.gd
+++ scenes/checkpoint/checkpoint.gd
@@ -5,11 +5,7 @@ signal player_walked_through(angle)
 @export var has_triggered = false
 @export var target_angle: float = 0.0
 
-
-
 func _on_static_body_2d_body_exited(body: Node2D) -> void:
-	print("Im here")
 	if body.is_in_group("player") and !has_triggered:
 		player_walked_through.emit(target_angle)
 		has_triggered = true
-		
