f87173646d4bdbebdb14d08b3f36d8cd67a136aa
Andrea Polanco <alpm9614@hotmail.com>
2026-02-16 20:19:11 -0400

    Added stamina bar logic

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
+@onready var stamina_bar: ProgressBar = $"../StaminaBar/ProgressBar"
 
 func _physics_process(delta: float) -> void:
 	
@@ -13,8 +13,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if stamina_bar.value > 1 and Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		stamina_bar.reduce_after_jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
