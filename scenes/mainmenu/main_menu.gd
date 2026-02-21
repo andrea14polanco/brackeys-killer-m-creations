872b79b63ac7ac37f775a388db0f5f7c18eb883c
Killer M. Niko <chase.o.hester@gmail.com>
2026-02-21 12:53:12 -0700

    assets updates and cutscene

--- scenes/mainmenu/main_menu.gd
+++ scenes/mainmenu/main_menu.gd
@@ -1,4 +1,10 @@
 extends Control
 
+var tween = null
+
 func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/cutscenes/intro_cut_scene.tscn")
+	##get_tree().change_scene_to_file("res://scenes/cutscenes/intro_cut_scene.tscn")
+	tween = create_tween()
+	tween.tween_property($Camera2D, "position", Vector2(1986, 360), 5.0)
+	await tween.finished
+	$Intro_cut_scene.start_chat()
