024488af8f2c4dfe175fc838accf29ad7159c417
Andrea Polanco <alpm9614@hotmail.com>
2026-02-20 21:19:27 -0400

    Added animation logic

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,12 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const CLIMBING_SPEED = 100
 
 @onready var stamina_bar: ProgressBar = $"../StaminaBar/ProgressBar"
 
 var is_on_ladder: bool = false
 var gravity: Vector2 = Vector2(0.0, 980.0)
-
+var facing_right = true
 
 
 func _physics_process(delta: float) -> void:
@@ -16,7 +17,7 @@ func _physics_process(delta: float) -> void:
 	if is_on_ladder:
 		if vertical_direction:
 			gravity = Vector2 (0, 0)
-			velocity.y = vertical_direction * 50
+			velocity.y = vertical_direction * CLIMBING_SPEED
 		else:
 			velocity.y = 0
 	# Add the gravity.
@@ -24,7 +25,7 @@ func _physics_process(delta: float) -> void:
 		velocity += gravity * delta
 
 	# Handle jump.
-	if stamina_bar.value > 1 and Input.is_action_just_pressed("jump") and is_on_floor():
+	if stamina_bar.value > 1 and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
 		stamina_bar.reduce_after_jump()
 
@@ -38,7 +39,24 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-
+	# Sprite direction and smoke pos
+	
+	if is_on_ladder:
+		$AnimatedSprite2D.play("climb")
+		if velocity.y == 0:
+			$AnimatedSprite2D.play("idle_climb")
+	elif velocity.x > 0:
+		$AnimatedSprite2D.play("walk_right")
+		facing_right = true
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("walk_left")
+		facing_right = false
+	elif velocity.x == 0 and not is_on_ladder:
+		if facing_right:
+			$AnimatedSprite2D.play("idle_right")
+		else:
+			$AnimatedSprite2D.play("idle_left")
+	print(velocity)
 func _on_stairs_area_entered(area: Area2D) -> void:
 	print("ESTOY AQUI")
 	is_on_ladder = true
