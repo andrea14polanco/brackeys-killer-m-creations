fb6cb89a7143214c1512d23fae54db477b3671c9
Killer M. Niko <chase.o.hester@gmail.com>
2026-02-22 02:46:56 -0700

    update1

--- scenes/levels/titanic/bulkhead.gd
+++ scenes/levels/titanic/bulkhead.gd
@@ -1,8 +1,9 @@
 extends StaticBody2D
 
-func set_all_collision_shapes_enabled(root: Node, enabled: bool) -> void:
+func set_all_collision_shapes_enabled(enabled: bool) -> void:
+	var root = self
 	for child in root.get_children():
 		if child is CollisionShape2D:
-			child.disabled = not enabled
+			child.set_deferred("disabled", not enabled)
 		else:
-			set_all_collision_shapes_enabled(child, enabled)
+			set_all_collision_shapes_enabled(enabled)
