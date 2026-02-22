530fd3dbcde75343c3511641a78d8ccc616a018e
Andrea Polanco <alpm9614@hotmail.com>
2026-02-21 23:34:15 -0400

    fixed_stairs

--- scenes/stairs/stairs.gd
+++ scenes/stairs/stairs.gd
@@ -1,33 +1,53 @@
 extends StaticBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+const ROTATION_LIMIT = 45
+
 var is_player_on_stairs
+var is_rotated
+
+var player_nearby: bool = false
+var is_flat: bool = false
+
 
 func _ready() -> void:
-	pass#collision_shape_2d.set_deferred("disabled", true)
+	collision_shape_2d.set_deferred("disabled", true)
 
 func _physics_process(delta: float) -> void:
-	pass
-		
+	var titanic_angle = abs(GameManager.titanic.global_rotation_degrees)
+	is_rotated = titanic_angle > ROTATION_LIMIT
 
-
-func _on_player_player_take_stairs() -> void:
-	is_player_on_stairs = true
-	collision_shape_2d.set_deferred("disabled", false)
-
-func _on_player_in_stairs_area_detector_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		body.is_on_stairs = true
-		collision_shape_2d.set_deferred("disabled", true)
-
-
-func _on_player_at_stairs_down_area_detector_body_exited(body: Node2D) -> void:
-	if not is_player_on_stairs:
-		collision_shape_2d.set_deferred("disabled", true)
-		is_player_on_stairs = false
-
-
-func _on_player_at_stairs_up_area_detector_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		body.is_on_stairs = true
+	if is_rotated:
 		collision_shape_2d.set_deferred("disabled", false)
+		return
+
+	var player: CharacterBody2D = GameManager.player
+	if player == null or player.is_on_ladder:
+		return
+
+	if not player_nearby:
+		collision_shape_2d.set_deferred("disabled", true)
+		return
+
+	# Press UP → walk onto the ramp
+	if Input.is_action_just_pressed("move_up"):
+		collision_shape_2d.set_deferred("disabled", false)
+
+	# Falling onto it → land on the ramp
+	if not player.is_on_floor() and player.velocity.y > 0:
+		collision_shape_2d.set_deferred("disabled", false)
+
+
+func _on_detection_zone_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_nearby = true
+		if not is_rotated:
+			collision_shape_2d.set_deferred("disabled", true)
+
+
+func _on_detection_zone_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_nearby = false
+		if not is_rotated:
+			collision_shape_2d.set_deferred("disabled", true)
--- scenes/stairs/stairs_platform.gd
+++ scenes/stairs/stairs_platform.gd
@@ -1,7 +1,6 @@
 extends StaticBody2D
 
-
-@onready var col: CollisionObject2D = $StairCol
+@onready var col: CollisionShape2D = $StairCol
 @onready var detection_area: Area2D = $DetectionZone
 
 const LIMIT_DEGREES = 25
@@ -11,28 +10,58 @@ var player_using_stair: bool = false
 var is_flat: bool = false
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	col.disabled = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	_update_flat_state()
+
 	if is_flat:
+		col.disabled = false
+		player_using_stair = false
 		return
-		
-	var player:CharacterBody2D = GameManager.player
-	
+
+	var player: CharacterBody2D = GameManager.player
 	if player == null:
 		return
-	
+
 	if player.is_on_ladder:
 		return
 
+	if not player_nearby:
+		if not player_using_stair:
+			col.disabled = true
+		return
+
+	# Press UP near stairs → enable collision, player walks on the ramp
+	if Input.is_action_just_pressed("move_up") and not player_using_stair:
+		player_using_stair = true
+		col.disabled = false
+
+	# Jumping near stairs → enable collision so player can land on it
+	if not player_using_stair and not player.is_on_floor() and player.velocity.y > 0:
+		col.disabled = false
+
+	# On the stair and jumps → let them leave
+	if player_using_stair and Input.is_action_just_pressed("jump"):
+		player_using_stair = false
+		col.disabled = true
+
+
+func _update_flat_state() -> void:
+	var angle = abs(fmod(global_rotation_degrees, 180.0))
+	is_flat = angle < LIMIT_DEGREES or angle > (180.0 - LIMIT_DEGREES)
+
 
 func _on_detection_zone_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("player"):
+		player_nearby = true
 
 
 func _on_detection_zone_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("player"):
+		player_nearby = false
+		player_using_stair = false
+		if not is_flat:
+			col.disabled = true
